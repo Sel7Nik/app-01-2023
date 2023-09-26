a6b25b8b28f4323e3eba3b9ba6c5919bcda60606
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2023-09-26 21:20:26 +0300

    2023 09 26 React Kamasutra componentDidMount()

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,70 +1,69 @@
+import React, { Component } from 'react'
 import styles from './users.module.css'
 import './users.css'
 import userAvatar from './../../assets/image/user-avatar.png'
 import axios from 'axios'
-import Button from './../Button/Button'
 
-export const Users = (props) => {
-  const getUsers = () => {
-    if (props.users.length === 0) {
+export class Users extends Component {
+  componentDidMount() {
+    if (this.props.users.length === 0) {
       axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response) => {
-        props.setUsers(response.data.items)
+        this.props.setUsers(response.data.items)
       })
     }
   }
-  return (
-    <div className={styles.section}>
-      <h3 className={`${styles['section-title']} ${'title'}`}>Users</h3>
-      {/* <button onClick={getUsers}>get Users</button> */}
-      <div>
-        <Button func={getUsers}>getUsers</Button>
-      </div>
-      {/* <h3 className={styles['section-title']}>Users</h3> */}
-      {props.users.map((user) => (
-        <div className={styles.users} key={user.id}>
-          <span>
-            <div className={styles.photo}>
-              <img
-                className={styles.img}
-                src={user.photos.small != null ? user.photos.small : userAvatar}
-                alt="img"
-                style={{ widht: '35px' }}
-              />
-            </div>
-            <div>
-              {user.followed ? (
-                <button
-                  onClick={() => {
-                    props.unFollow(user.id)
-                  }}
-                >
-                  unfollow
-                </button>
-              ) : (
-                <button
-                  onClick={() => {
-                    props.follow(user.id)
-                  }}
-                >
-                  follow
-                </button>
-              )}
-            </div>
-          </span>
 
-          <div className={styles.descr}>
-            <div className={styles.name}>
-              <div className={styles.fullName}>{user.name}</div>
-              <div>{'user.status'}</div>
-            </div>
+  render() {
+    return (
+      <div className={styles.section}>
+        <h3 className={`${styles['section-title']} ${'title'}`}>Users</h3>
 
-            <div className={styles.location}>
-              <div>{'user.location.country'}</div>
-              <div>{'user.location.city'}</div>
+        {this.props.users.map((user) => (
+          <div className={styles.users} key={user.id}>
+            <span>
+              <div className={styles.photo}>
+                <img
+                  className={styles.img}
+                  src={user.photos.small != null ? user.photos.small : userAvatar}
+                  alt="img"
+                  style={{ widht: '35px' }}
+                />
+              </div>
+              <div>
+                {user.followed ? (
+                  <button
+                    onClick={() => {
+                      this.props.unFollow(user.id)
+                    }}
+                  >
+                    unfollow
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      this.props.follow(user.id)
+                    }}
+                  >
+                    follow
+                  </button>
+                )}
+              </div>
+            </span>
+
+            <div className={styles.descr}>
+              <div className={styles.name}>
+                <div className={styles.fullName}>{user.name}</div>
+                <div>{'user.status'}</div>
+              </div>
+
+              <div className={styles.location}>
+                <div>{'user.location.country'}</div>
+                <div>{'user.location.city'}</div>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </div>
-  )
+        ))}
+      </div>
+    )
+  }
 }
